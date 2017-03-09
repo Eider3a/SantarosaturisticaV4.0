be9022e39de0b0859c76b825a85e3f9cd65aa4d9
Eider3a <eider.a@hotmail.com>
2017-03-09 10:39:17 -0500

    V2.0

---
+++ app/src/main/java/com/eider/santarosaturistica/DemoActivity.java
@@ -0,0 +1,53 @@
+package com.eider.santarosaturistica;
+
+//import android.app.FragmentManager;
+//import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+//        Manejo de fragments dinamicos.
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+
+        hotel1fragment fragment=new hotel1fragment();
+        ft.add(android.R.id.content,fragment).commit();//Quitar support.v4 en el demo_activity
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu2,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();//Siempre hay que colocar la transacion si se cambia de metodo.
+
+        switch (id){
+            case R.id.mfrag1:
+                hotel1fragment frag=new hotel1fragment();
+                ft.replace(android.R.id.content,frag).commit();
+                break;
+
+            case R.id.mfrag2:
+                hotel2fragment frag2=new hotel2fragment();
+                ft.replace(android.R.id.content,frag2).commit();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
